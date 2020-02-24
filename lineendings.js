073271a8816bf726ead520a14ca9f643dafa06f0
Courtney Owen <cowen@microsoft.com>
2020-02-23 16:30:29 -0800

    add lineendings test

---
+++ lineendings.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const path = require('path');
+
+getLineEndings();
+
+function getLineEndings() {
+
+        var textResult = fs.readFileSync(path.join(__dirname, './dist/taskpane.js'), 'utf8');
+            var total = [];
+            var lines = textResult.split(/\r\n|\n|\r/);
+            for (var i = 0; i < lines.length; i++) {
+                total.push(lines[i].length + 1);
+            }
+            console.log(total);
+
+}
