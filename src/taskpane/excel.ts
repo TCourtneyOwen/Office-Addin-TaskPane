6db884b4e241b2bd1d93987589dc62d233ab3d69
Courtney Owen <cowen@microsoft.com>
2020-04-02 08:22:55 -0700

    Add logic to deal with invalid entries

--- src/taskpane/excel.ts
+++ src/taskpane/excel.ts
@@ -18,27 +18,31 @@ Office.onReady(info => {
 export async function run() {
   try {
     await Excel.run(async context => {
-      /**
-       * Insert your Excel code here
-       */
+
       const range = context.workbook.getSelectedRange();
 
-      // Read the range address
+      // Load range values
       range.load("values");
       await context.sync();
-      const country = range.values[0][0];
+      const countries = range.values;
 
-      if (country !== "") {
-        const dataByCountry = await getCovidDataByCountry(country);
-        const countryData = Object.getOwnPropertyNames(dataByCountry);
-        if(countryData[0] !== "dt") {
-          await addTableForCountry(dataByCountry, context);
-          await addChart(context);
-        }else {
-          range.values = [[`${country} is not a valid country name`]];
-          await context.sync();
+      for (let i = 0; i < countries.length; i++) {
+        if (countries[i].toString() === "") {
+          range.values[i] = [[`No country named entered`]];
+        } else {
+          const dataByCountry = await getCovidDataByCountry(countries[i]);
+          const country = countries[i].toString().toLowerCase();
+          const found = dataByCountry[Object.keys(dataByCountry).find(key => key.toLowerCase() === country.toLowerCase())];
+
+          if (found) {
+            await addTableForCountry(dataByCountry, context);
+            await addChart(context);
+          } else {
+            range.values[i] = [[`${countries[i]} is not a valid country name`]];
+          }
         }
       }
+      await context.sync();
     });
   } catch (error) {
     console.error(error);
@@ -49,6 +53,7 @@ export async function getCovidData(): Promise<any> {
   return new Promise<object>(async (resolve, reject) => {
     const serverResponse: any = {};
     try {
+
       const apiUrl: string = `https://covid2019-api.herokuapp.com/v2/total`;
       const response = await fetch(apiUrl);
       serverResponse["status"] = response.status;
@@ -86,38 +91,25 @@ async function getCovidDataForAllCountries(): Promise<any> {
   });
 }
 
-async function getCovidDataByCountry(country: string): Promise<any> {
+async function getCovidDataByCountry(country: any[][]): Promise<any> {
   return new Promise<object>(async (resolve, reject) => {
     const serverResponse: any = {};
     try {
       // Get data for country and add to databyCountry
-      const dataByCountryApiUrl = `https://covid2019-api.herokuapp.com/country/${country}`;
-      const response = await fetch(dataByCountryApiUrl);
-      serverResponse["status"] = response.status;
-      const text = await response.text();
-      resolve(JSON.parse(text));
+      let databyCountry: Object[] = []
+      for (let i = 0; i < country.length; i++) {
+        const dataByCountryApiUrl = `https://covid2019-api.herokuapp.com/country/${country[i]}`;
+        const response = await fetch(dataByCountryApiUrl);
+        serverResponse["status"] = response.status;
+        const text = await response.text();
+        const countryData = JSON.parse(text);
+        databyCountry.push(countryData);
+      }
+      resolve(databyCountry);
     } catch (err) {
-      serverResponse["status"] = err;
-      reject(serverResponse);
+      reject(err);
     }
   });
-}
-
-async function addTable(data: any, context) {
-  var sheet = context.workbook.worksheets.getItem("Sheet1");
-  var covidTable = sheet.tables.add("A1:D1", true /*hasHeaders*/);
-  covidTable.name = "CovidTable";
-  covidTable.getHeaderRowRange().values = [["Country", "ComfirmedCases", "Recovered", "Deaths"]];
-
-  const countries = Object.values(data);
-  for (var key in countries) {
-    const country = countries[key];
-    const countryData = Object.getOwnPropertyNames(country)
-
-    covidTable.rows.add(null /*add rows to the end of the table*/, [
-      [countryData[0], country[countryData[0]].confirmed, country[countryData[0]].recovered, country[countryData[0]].deaths],
-    ]);
-  }
 }
 
 async function addTableForCountry(data: any, context): Promise<void | string> {
@@ -128,25 +120,28 @@ async function addTableForCountry(data: any, context): Promise<void | string> {
         currentTable.delete();
         await context.sync();
       }
-      
+
       var sheet = context.workbook.worksheets.getItem("Sheet1");
       var covidTable = sheet.tables.add("A1:D1", true /*hasHeaders*/);
       covidTable.name = "CovidTable";
       covidTable.getHeaderRowRange().values = [["Country", "ComfirmedCases", "Recovered", "Deaths"]];
 
-      const countryData = Object.getOwnPropertyNames(data);
+      const countries = Object.values(data);
+      for (var key in countries) {
+        const country = countries[key];
+        const countryData = Object.getOwnPropertyNames(country);
 
-      covidTable.rows.add(null /*add rows to the end of the table*/, [
-        [countryData[0], _.get(data, `${countryData[0]}.confirmed`), _.get(data, `${countryData[0]}.recovered`), _.get(data, `${countryData[0]}.deaths`)],
-      ]);
+        if (countryData[0] !== "dt") {
+          covidTable.rows.add(null /*add rows to the end of the table*/, [
+            [countryData[0], country[countryData[0]].confirmed, country[countryData[0]].recovered, country[countryData[0]].deaths],
+          ]);
+        }
+      }
 
       if (Office.context.requirements.isSetSupported("ExcelApi", "1.2")) {
         sheet.getUsedRange().format.autofitColumns();
         sheet.getUsedRange().format.autofitRows();
       }
-
-      const tableRange = covidTable.getRange();
-      tableRange.load('address');
       sheet.activate();
       await context.sync();
       resolve();
@@ -160,21 +155,41 @@ async function addChart(context): Promise<void> {
   return new Promise<void>(async (resolve, reject) => {
     try {
       const sheet = context.workbook.worksheets.getItem("Sheet1");
-      sheet.charts.load("count");
-      await context.sync();
-
-      if (sheet.charts.count > 0) {
-        return resolve();
+      const currentChart = context.workbook.worksheets.getItem("Sheet1").charts.getItemOrNullObject("Covid19Chart");
+      if (currentChart) {
+        currentChart.delete();
+        await context.sync();
       }
-     
-      const dataRange = sheet.getRange("A1:D2");
-      let chart = sheet.charts.add("3DColumnClustered", dataRange, "auto");
+
+      // Get table range
+      const currentTable = context.workbook.worksheets.getItem("Sheet1").tables.getItemOrNullObject("CovidTable");
+      const range = currentTable.getRange();
+      range.load("address");
+      await context.sync();
+      const updatedRange = range.address.slice(range.address.indexOf("!") + 1);
+      const dataRange = sheet.getRange(updatedRange);
+
+      // Get row count
+      const rows = currentTable.rows;
+      rows.load("count");
+      await context.sync();
+      const rowCount = rows.count;      
+
+      let chart = sheet.charts.add(rowCount < 5 ? "3DColumnClustered" : "3DColumnStacked", dataRange, "auto");
       chart.name = "Covid19Chart";
-      chart.title.text = "Covid19 Data";
+      chart.title.text = "COVID-19 Data";
       chart.legend.position = "right"
       chart.legend.format.fill.setSolidColor("white");
-      chart.dataLabels.format.font.size = 10;
-      chart.dataLabels.format.font.color = "black";
+
+      // Don't add data labels if too much data - makes chart look cluttered
+      if (rowCount < 5) {
+        chart.dataLabels.format.font.size = 12;
+        chart.dataLabels.format.font.color = "black";
+        chart.dataLabels.textOrientation = 90;
+      }
+
+      chart.height = 300;
+      chart.width = 500;
 
       await context.sync();
       resolve();
